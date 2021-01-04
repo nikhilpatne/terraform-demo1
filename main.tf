5740b4a9de4d8020146cee940fd84b1f6476cd3d
nikhil.patne <nikhil.patne@gslab.com>
2021-01-04 18:38:01 +0530

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "My_instance" {
+    ami = "ami-0a91cd140a1fc148a"
+    instance_type = "t2.micro"
+    security_groups = ["default"]
+    key_name = "nikhil-ec2"
+    tags = {
+        Name = "Nikhil-VM01"
+    }
+}
